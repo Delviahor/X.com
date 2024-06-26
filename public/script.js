25a2cfeaddcbe92e2bb1b2b944300cafa38f19e9
Samantha <samantha.gabriel.borrego@gmail.com>
2024-06-25 23:17:35 -0500

    Se ha agregado la tabla transferencia a la DB y se intenta agregar la funcion Transferencia, mostrada en home.html

--- public/script.js
+++ public/script.js
@@ -4,52 +4,48 @@ document.getElementById('login-form')?.addEventListener('submit', function(event
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
 
-  // Enviar las credenciales al servidor utilizando fetch
   fetch('/login', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`
   })
   .then(response => {
-      if (!response.ok) {
-          throw new Error('Network response was not ok');
-      }
-      return response.json();
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.text();
   })
   .then(data => {
-      if (data.message === 'Inicio de sesión exitoso') {
-          alert("¡Inicio de sesión exitoso!");
-          window.location.href = `/home?username=${encodeURIComponent(username)}`;
-      } else {
-          throw new Error('Credenciales incorrectas');
-      }
+    if (data === 'Inicio de sesión exitoso') {
+      alert("Login successful!");
+      window.location.href = `/home?username=${encodeURIComponent(username)}`;
+    } else {
+        throw new Error('Credenciales incorrectas');
+    }
   })
   .catch(error => {
-      if (error.message === 'Credenciales incorrectas') {
-          const errorMessage = document.createElement('p');
-          errorMessage.textContent = "Nombre de usuario o contraseña incorrectos.";
-          document.body.appendChild(errorMessage);
-      } else {
-          const errorMessage = document.createElement('p');
-          errorMessage.textContent = "Error en el servidor.";
-          document.body.appendChild(errorMessage);
-          console.error('Error:', error);
-      }
+    if (error.message === 'Credenciales incorrectas') {
+      const errorMessage = document.createElement('p');
+      errorMessage.textContent = "Nombre de usuario o contraseña incorrectos.";
+      document.body.appendChild(errorMessage);
+    } else {
+      const errorMessage = document.createElement('p');
+      errorMessage.textContent = "Nombre de usuario o contraseña incorrectos. (2)";
+      document.body.appendChild(errorMessage);
+      console.error('Error:', error);
+    }
   });
 });
 
-// Manejar el evento de clic del botón de registro
 document.getElementById('register-button')?.addEventListener('click', function() {
   window.location.href = 'register.html';
 });
 
-// Manejar el evento de envío del formulario de registro
 document.getElementById('register-form')?.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  // Validación de campos de nombres y apellidos
   const firstName = document.getElementById('first-name').value;
   const secondName = document.getElementById('second-name').value;
   const firstSurname = document.getElementById('first-surname').value;
@@ -58,8 +54,8 @@ document.getElementById('register-form')?.addEventListener('submit', function(ev
   const regexUppercase = /[A-Z]/;
 
   if (regexUppercase.test(firstName) || regexUppercase.test(secondName) || regexUppercase.test(firstSurname) || regexUppercase.test(secondSurname)) {
-      alert('Los campos de nombres y apellidos deben estar en minúsculas.');
-      return;
+    alert('Los campos de nombres y apellidos deben estar en minúsculas.');
+    return;
   }
 
   const formData = new FormData(this);
@@ -74,18 +70,14 @@ document.getElementById('register-form')?.addEventListener('submit', function(ev
     .catch(error => console.error('Error:', error));
 });
 
-// Manejar los checkboxes para "No tengo"
-// Segundo nombre
 document.getElementById('no-second-name')?.addEventListener('change', function(event) {
   document.getElementById('second-name').disabled = event.target.checked;
 });
 
-// Segundo apellido
 document.getElementById('no-second-surname')?.addEventListener('change', function(event) {
   document.getElementById('second-surname').disabled = event.target.checked;
 });
 
-// Esperar a que el documento esté completamente cargado
 document.addEventListener('DOMContentLoaded', function() {
   const urlParams = new URLSearchParams(window.location.search);
   const username = urlParams.get('username');
@@ -96,16 +88,37 @@ document.addEventListener('DOMContentLoaded', function() {
           usernamePlaceholder.textContent = username;
       }
 
-      fetch(`/home?username=${encodeURIComponent(username)}`)
-          .then(response => response.text())
-          .then(html => {
-              document.body.innerHTML = html;
-          })
-          .catch(error => {
-              console.error('Error:', error);
-              const errorMessage = document.createElement('p');
-              errorMessage.textContent = "Error al cargar la página de inicio.";
-              document.body.appendChild(errorMessage);
-          });
+      fetch(`/get-saldo?username=${encodeURIComponent(username)}`)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('saldo-placeholder').textContent = data.saldo.toFixed(2);
+        })
+        .catch(error => console.error('Error:', error));
   }
+});
+
+document.getElementById('transfer-form')?.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const usernameOrigen = new URLSearchParams(window.location.search).get('username');
+  const usernameDestino = document.getElementById('dest-username').value;
+  const monto = document.getElementById('amount').value;
+
+  fetch('/transfer', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: `usernameOrigen=${encodeURIComponent(usernameOrigen)}&usernameDestino=${encodeURIComponent(usernameDestino)}&monto=${encodeURIComponent(monto)}`
+  })
+  .then(response => response.text())
+  .then(data => {
+    alert(data);
+    if (data === 'Transferencia exitosa') {
+      location.reload();
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    alert('Error al realizar la transferencia');
+  });
 });
--- server/server.js
+++ server/server.js
@@ -6,13 +6,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Configurar body-parser para manejar solicitudes POST
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Servir archivos estáticos desde el directorio 'public'
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// Conectar a la base de datos SQLite
 let db = new sqlite3.Database(path.join(__dirname, 'database.db'), (err) => {
     if (err) {
         console.error('Error al conectar a la base de datos:', err.message);
@@ -21,44 +18,41 @@ let db = new sqlite3.Database(path.join(__dirname, 'database.db'), (err) => {
     }
 });
 
-// Ruta para la página de inicio
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
-// Ruta para la página de inicio de sesión
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'login.html'));
 });
 
-// Ruta para la página de registro
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'register.html'));
 });
 
-// Ruta para la página principal después del inicio de sesión
 app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'home.html'));
+});
+
+app.get('/saldo', (req, res) => {
     const { username } = req.query;
     const query = `SELECT saldo FROM usuarios WHERE nombre_usuario = ?`;
     db.get(query, [username], (err, row) => {
         if (err) {
             console.error('Error al consultar la base de datos:', err.message);
-            res.status(500).send('Error en el servidor');
+            res.status(500).send({ error: 'Error en el servidor' });
+        } else if (row) {
+            res.send({ saldo: row.saldo });
         } else {
-            res.send(`
-                <h2>Bienvenido, ${username}!</h2>
-                <p>¡Has iniciado sesión correctamente!</p>
-                <p>Tu saldo actual es: ${row.saldo}</p>
-            `);
+            res.status(404).send({ error: 'Usuario no encontrado' });
         }
     });
 });
 
-// Manejar el envío del formulario de registro
 app.post('/register', (req, res) => {
     const { 'first-name': firstName, 'second-name': secondName, 'first-surname': firstSurname, 'second-surname': secondSurname, email, username, password } = req.body;
 
-    const query = `INSERT INTO usuarios (primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, correo, nombre_usuario, contrasena, saldo) VALUES (?, ?, ?, ?, ?, ?, ?, 1000.0)`;
+    const query = `INSERT INTO usuarios (primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, correo, nombre_usuario, contrasena, saldo) VALUES (?, ?, ?, ?, ?, ?, ?, 0)`;
 
     db.run(query, [firstName, secondName, firstSurname, secondSurname, email, username, password], function(err) {
         if (err) {
@@ -69,10 +63,8 @@ app.post('/register', (req, res) => {
     });
 });
 
-// Manejar el envío del formulario de inicio de sesión
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-    console.log('Intentando iniciar sesión con:', username, password);
     const query = `SELECT * FROM usuarios WHERE nombre_usuario = ? AND contrasena = ?`;
 
     db.get(query, [username, password], (err, row) => {
@@ -80,16 +72,64 @@ app.post('/login', (req, res) => {
             console.error('Error al consultar la base de datos:', err.message);
             res.status(500).send('Error en el servidor');
         } else if (row) {
-            console.log('Usuario encontrado:', row);
-            res.send(JSON.stringify({ message: 'Inicio de sesión exitoso', saldo: row.saldo }));
+            res.send('Inicio de sesión exitoso');
         } else {
-            console.log('Nombre de usuario o contraseña incorrectos');
             res.status(401).send('Nombre de usuario o contraseña incorrectos');
         }
     });
 });
 
-// Iniciar el servidor
+app.post('/transfer', (req, res) => {
+    const { username, destUsername, amount } = req.body;
+    const amountNumber = parseFloat(amount);
+
+    if (amountNumber <= 0) {
+        res.send({ success: false, message: 'El monto debe ser mayor a cero' });
+        return;
+    }
+
+    db.serialize(() => {
+        db.get(`SELECT saldo FROM usuarios WHERE nombre_usuario = ?`, [username], (err, row) => {
+            if (err) {
+                console.error('Error al consultar la base de datos:', err.message);
+                res.status(500).send({ success: false, message: 'Error en el servidor' });
+                return;
+            }
+
+            if (!row || row.saldo < amountNumber) {
+                res.send({ success: false, message: 'Saldo insuficiente' });
+                return;
+            }
+
+            db.run(`UPDATE usuarios SET saldo = saldo - ? WHERE nombre_usuario = ?`, [amountNumber, username], (err) => {
+                if (err) {
+                    console.error('Error al actualizar el saldo del remitente:', err.message);
+                    res.status(500).send({ success: false, message: 'Error en el servidor' });
+                    return;
+                }
+
+                db.run(`UPDATE usuarios SET saldo = saldo + ? WHERE nombre_usuario = ?`, [amountNumber, destUsername], (err) => {
+                    if (err) {
+                        console.error('Error al actualizar el saldo del destinatario:', err.message);
+                        res.status(500).send({ success: false, message: 'Error en el servidor' });
+                        return;
+                    }
+
+                    db.get(`SELECT saldo FROM usuarios WHERE nombre_usuario = ?`, [username], (err, row) => {
+                        if (err) {
+                            console.error('Error al consultar el saldo actualizado:', err.message);
+                            res.status(500).send({ success: false, message: 'Error en el servidor' });
+                            return;
+                        }
+
+                        res.send({ success: true, newSaldo: row.saldo });
+                    });
+                });
+            });
+        });
+    });
+});
+
 app.listen(port, () => {
     console.log(`Servidor iniciado en http://localhost:${port}`);
 });
